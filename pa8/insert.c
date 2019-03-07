18e013b37ece1fda78e55d09890350d309dbaca8
Joe Gibbs Politz <joe.politz@gmail.com>
2019-03-06 18:12:59 -0800

    update insert

--- pa8/insert.c
+++ pa8/insert.c
@@ -1,13 +1,11 @@
-
-// Fix the bug in insert so that it continues to work, and so that
-// running with the given main doesn't cause a valgrind error
-// for an out-of-bounds memory access.
-// Run with
+// Fix the bug in insert so that it continues to work, and so that running with
+// the given main doesn't cause a valgrind error for an out-of-bounds memory
+// access.  Run with
 //
 // 		make insert
 //
-// This insert can assume that the last element in the array elts
-// can be overwritten. So an input array like
+// This insert can assume that the last element in the array elts can be
+// overwritten and lost. So for an input array like
 //
 // 4, 5, 6, 7
 //
@@ -15,9 +13,8 @@
 //
 // 4, 5, 44, 6
 //
-// Sorry for the confusion about this! You don't need to make new space
-// and return the new array, just fix the memory error that results
-// from trying to do the above.
+// You *don't* need to make new space and return the new array, just fix the
+// memory error that results from trying to do the above.
 void insert(int* elts, int size, int index, int toInsert) {
 	int i;
   for(i = size - 1; i >= index; i -= 1) {
@@ -26,3 +23,4 @@ void insert(int* elts, int size, int index, int toInsert) {
   elts[index] = toInsert;
   return;
 }
+
